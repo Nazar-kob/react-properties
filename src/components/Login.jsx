1a94007d97b932ab716befb204527a1ddf0d4bcd
nazarKobryn <kobrinnazar05@gmail.com>
2022-10-31 15:39:43 +0200

    control input recommend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   console.log("render");
+
+  const [username, setUsetname] = useState("");
+  const [password, setPassword] = useState("");
+
   function handelFormSubmit(event) {
     event.preventDefault();
     const user = {
-      username: event.target.username.value,
-      password: event.target.password.value,
+      username: username,
+      password: password,
     };
-    console.log(user);
+    setUsetname("");
+    setPassword("");
   }
-  console.log(handelFormSubmit);
   return (
     <>
       <h1>Login form</h1>
       <form onSubmit={handelFormSubmit} action="#">
         <label>
           User name
-          <input type="text" name="username" />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              setUsetname(e.target.value);
+            }}
+          />
         </label>
 
         <label>
           User password
-          <input type="password" name="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <button type="submit">Submit</button>
       </form>
