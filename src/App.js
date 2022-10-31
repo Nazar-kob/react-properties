0274e8bb26e8c2f3322a825955849cc30ebcff4d
nazarKobryn <kobrinnazar05@gmail.com>
2022-10-31 15:18:57 +0200

    not control input (bad)

--- src/App.js
+++ src/App.js
@@ -2,32 +2,31 @@ import React, { useState } from "react";
 import './App.css';
 // import Note from "./components/Note";
 // import RandomNumber from "./components/RandomNumber";
-import Counter from "./components/Counter";
-import Button from "./components/Button";
+// import Counter from "./components/Counter";
+// import Button from "./components/Button";
 
-const textBtns = ["Hi click me", "Press me", "why not press me", "Hit me or I hit you", "Please change count!"]
+// const textBtns = ["Hi click me", "Press me", "why not press me", "Hit me or I hit you", "Please change count!"]
 // import Persons from "./components/Persons";
+import Login from "./components/Login";
 
 
 function App() {
 
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
-  const incrementCount = () => {
-    setCounter(counter + 1)
-  }
+  // const incrementCount = () => {
+  //   setCounter(counter + 1)
+  // }
 
-  const countBtns = textBtns.map((text, index) => {
-    return <Button key={index} onClick={incrementCount} text={text} />
-  })
-  const styleResetBtn = { backgroundColor: 'green' }
+  // const countBtns = textBtns.map((text, index) => {
+  //   return <Button key={index} onClick={incrementCount} text={text} />
+  // })
+  // const styleResetBtn = { backgroundColor: 'green' }
 
 
   return (
     <div className="App">
-      <Counter count={counter} />
-      {countBtns}
-      {counter > 0 && <div style={styleResetBtn}> <button onClick={() => setCounter(0)}>Reset sount</button> </div>}
+      <Login />
     </div>
   );
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Login = () => {
+  console.log("render");
+  function handelFormSubmit(event) {
+    event.preventDefault();
+    const user = {
+      username: event.target.username.value,
+      password: event.target.password.value,
+    };
+    console.log(user);
+  }
+  console.log(handelFormSubmit);
+  return (
+    <div>
+      <h1>Login form</h1>
+      <form onSubmit={handelFormSubmit} action="#">
+        <label>
+          User name
+          <input type="text" name="username" />
+        </label>
+
+        <label>
+          User password
+          <input type="password" name="password" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
