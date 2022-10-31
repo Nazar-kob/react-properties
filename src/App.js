fbd0151de1ed10eb0740137d6f485cba78396324
nazarKobryn <kobrinnazar05@gmail.com>
2022-10-31 14:10:49 +0200

    hide button

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import React, { useState } from "react";
 import './App.css';
 // import Note from "./components/Note";
 // import RandomNumber from "./components/RandomNumber";
-// import Counter from "./components/Counter";
-// import Button from "./components/Button";
+import Counter from "./components/Counter";
+import Button from "./components/Button";
 
-// const textBtns = ["Hi click me", "Press me", "why not press me", "Hit me or I hit you", "Please change count!"]
-import Persons from "./components/Persons";
+const textBtns = ["Hi click me", "Press me", "why not press me", "Hit me or I hit you", "Please change count!"]
+// import Persons from "./components/Persons";
 
 
 function App() {
 
-  // const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  // const incrementCount = () => {
-  //   setCounter(counter + 1)
-  // }
+  const incrementCount = () => {
+    setCounter(counter + 1)
+  }
 
-  // const countBtns = textBtns.map((text, index) => {
-  //   return <Button key={index} onClick={incrementCount} text={text} />
-  // })
+  const countBtns = textBtns.map((text, index) => {
+    return <Button key={index} onClick={incrementCount} text={text} />
+  })
+  const styleResetBtn = { backgroundColor: 'green' }
 
 
   return (
     <div className="App">
-      <Persons />
+      <Counter count={counter} />
+      {countBtns}
+      {counter > 0 && <div style={styleResetBtn}> <button onClick={() => setCounter(0)}>Reset sount</button> </div>}
     </div>
   );
 }
