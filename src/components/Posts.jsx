13b9cc48ec7a2cdbc53ba30dc86b993a1ff038ef
nazarKobryn <kobrinnazar05@gmail.com>
2022-11-01 10:21:05 +0200

    use async in useEffect

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,18 +2,34 @@ import React, { useState, useEffect } from "react";
 import Post from "./Post";
 import Spinner from "./Spinner/Spinner";
 
+const API_URL = "https://jsonplaceholder.typicode.com/posts";
+
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => setPosts(json))
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
+    const fetchData = async () => {
+      try {
+        const res = await fetch(API_URL);
+        const posts = await res.json();
+        setPosts(posts);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   fetch(API_URL)
+  //     .then((response) => response.json())
+  //     .then((json) => setPosts(json))
+  //     .catch((error) => setError(error.message))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
 
   if (error) {
     return <h1>{error}</h1>;
